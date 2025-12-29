513cc78ca9e791bb2b5a8b53bb0287bb5ce0ce09
arnaudbonnafoux <bonnafouxarnaud@gmail.com>
2025-12-29 17:03:16 +0100

    modif titres images et galeries

--- script.js
+++ script.js
@@ -1,33 +1,33 @@
 // Liste des images - Galerie 1
 const images1 = [
-  { src: 'images/visuel_1.webp', title: 'Étranger à tout' },
-  { src: 'images/visuel_2.webp', title: 'Éclats du Néant' },
-  { src: 'images/visuel_3.webp', title: 'Topographie du Rêve' },
-  { src: 'images/visuel_4.webp', title: 'Fragment de Temps' },
-  { src: 'images/visuel_6.webp', title: 'Lignes d\'Errance' },
-  { src: 'images/visuel_7.webp', title: 'Spectres de Lumière' },
-  { src: 'images/visuel_8.webp', title: 'Respiration du Vide' },
-  { src: 'images/visuel_9.webp', title: 'Flux Intemporel' },
-  { src: 'images/visuel_10.webp', title: 'Matière du Souffle' },
-  { src: 'images/visuel_11.webp', title: 'Échos du Silence' },
-  { src: 'images/visuel_13.webp', title: 'Cartographie de l\'Invisible' },
-  { src: 'images/visuel_14.webp', title: 'Osmose des Couleurs' },
+  { src: 'images/visuel_1.webp', title: 'Alienus Omnium' },
+  { src: 'images/visuel_2.webp', title: 'Splendores Nihili' },
+  { src: 'images/visuel_3.webp', title: 'Topographia Somni' },
+  { src: 'images/visuel_4.webp', title: 'Fragmentum Temporis' },
+  { src: 'images/visuel_6.webp', title: 'Viae Errantes' },
+  { src: 'images/visuel_7.webp', title: 'Spectra Lucis' },
+  { src: 'images/visuel_8.webp', title: 'Respiratio Vacui' },
+  { src: 'images/visuel_9.webp', title: 'Fluxus Aeternalis' },
+  { src: 'images/visuel_10.webp', title: 'Materia Spiritus' },
+  { src: 'images/visuel_11.webp', title: 'Echoes Silentii' },
+  { src: 'images/visuel_13.webp', title: 'Cartographia Invisibilis' },
+  { src: 'images/visuel_14.webp', title: 'Osmosis Colorum' },
 ];
 
 // Liste des images - Galerie 2
 const images2 = [
   //{ src: 'images/visuel_23.webp', title: 'Harmonie Chromatique 1' },
-  { src: 'images/visuel_24.webp', title: 'Danse des Teintes' },
-  { src: 'images/visuel_25.webp', title: 'Fusion Colorée' },
-  { src: 'images/visuel_26.webp', title: 'Murmures Chromatiques' },
-  { src: 'images/visuel_27.webp', title: 'Symphonie Silencieuse' },
-  { src: 'images/visuel_28.webp', title: 'Vibratos Chromatiques' },
-  { src: 'images/visuel_29.webp', title: 'Consonance Abstraite' },
-  { src: 'images/visuel_30.webp', title: 'Dissonance Harmonieuse' },
-  { src: 'images/visuel_31.webp', title: 'Crescendo Visuel' },
-  { src: 'images/visuel_33.webp', title: 'Vertiges Colorés' },
-  { src: 'images/visuel_34.webp', title: 'Resonance Infinie' },
-  { src: 'images/visuel_35.webp', title: 'Conclusion Onirique' },
+  { src: 'images/visuel_24.webp', title: 'Saltus Tincturarum' },
+  { src: 'images/visuel_25.webp', title: 'Fusio Colorata' },
+  { src: 'images/visuel_26.webp', title: 'Murmura Chromatica' },
+  { src: 'images/visuel_27.webp', title: 'Symphonia Tacita' },
+  { src: 'images/visuel_28.webp', title: 'Vibratio Chromatica' },
+  { src: 'images/visuel_29.webp', title: 'Consonantia Abstracta' },
+  { src: 'images/visuel_30.webp', title: 'Dissonantia Harmoniosa' },
+  { src: 'images/visuel_31.webp', title: 'Crescendo Visualis' },
+  { src: 'images/visuel_33.webp', title: 'Vertigines Coloratae' },
+  { src: 'images/visuel_34.webp', title: 'Resonantia Infinita' },
+  { src: 'images/visuel_35.webp', title: 'Conclusio Somnialis' },
 ];
 
 let currentGallery = 'gallery1';
@@ -119,11 +119,11 @@ tabGallery2.addEventListener('click', () => {
   
   // Changer le titre et le texte du bouton
   if (isGallery2Active) {
-    galleryTitle.textContent = 'Galerie "Révolutions Formelles"';
-    tabGallery2.textContent = 'Galerie Lignes d\'Errance';
+    galleryTitle.textContent = 'Galerie "Revolutiones Formales"';
+    tabGallery2.textContent = 'Galerie Viae Errantes';
   } else {
-    galleryTitle.textContent = 'Galerie "Lignes d\'Errance"';
-    tabGallery2.textContent = 'Galerie Révolutions Formelles';
+    galleryTitle.textContent = 'Galerie "Viae Errantes"';
+    tabGallery2.textContent = 'Galerie Revolutiones Formales';
   }
   
   // Mettre à jour la galerie actuelle
