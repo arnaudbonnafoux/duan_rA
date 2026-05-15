63d57ece32b4b8c479eb54f51526fa8aa9296f79
arnaudbonnafoux <bonnafouxarnaud@gmail.com>
2026-05-15 19:18:34 +0200

    Remplacer la playlist audio par respiratio_vacui et supprimer les anciens fichiers

--- script.js
+++ script.js
@@ -341,7 +341,7 @@ nextBtn.addEventListener('click', () => showNextImage());
 
 // Playlist audio
 const audioPlaylist = [
-  'audio/respiratio_vacui_v2.mp3'
+  'audio/respiratio_vacui.mp3'
 ];
 let currentAudioIndex = 0;
 
